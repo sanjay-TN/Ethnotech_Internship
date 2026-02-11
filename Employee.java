4ba36a23153036305227ba14156ad1e35159898a
sanjay-TN <sanjaysanjaytn77@gmail.com>
2026-02-11 11:05:31 +0530

    added example for inheritance

---
+++ Employee.java
@@ -0,0 +1,8 @@
+public class Employee {
+    private double salary = 100000;
+
+    public double get_sal() {
+        return salary;
+    }
+
+}
---
+++ Inheritance_Salary.java
@@ -0,0 +1,7 @@
+public class Inheritance_Salary extends Employee {
+    public static void main(String[] args) {
+        Inheritance_Salary s = new Inheritance_Salary();
+        System.out.println(s.get_sal());
+    }
+
+}
